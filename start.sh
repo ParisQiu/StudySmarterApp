b7726fbe6b4c71c891edc6447d7cc819609ff5f3
Paris Qiu <paris.qiu522@gmail.com>
2025-03-27 02:18:18 -0700

    Fix: register migrate explicitly in start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
+echo ">>> Setting environment..."
 export FLASK_APP=app.py
 export FLASK_ENV=production
 
-echo ">>> Setting environment..."
 echo ">>> Running migration with Python..."
-
 
 python3 -c "
 from app import app, db
-from flask_migrate import upgrade
+from flask_migrate import Migrate, upgrade
+migrate = Migrate(app, db)
 with app.app_context():
     upgrade()
 "
 
 echo ">>> Starting Gunicorn..."
-
 gunicorn app:app
